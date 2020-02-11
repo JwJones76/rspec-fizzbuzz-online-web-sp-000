de0491f3d9ce266fd2f2a49750cede9e6d5c9242
Jacob W Jones <jacobwadejones76@gmail.com>
2020-02-11 18:46:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@ def fizzbuzz(input)
 end 
  if input % 3 == input % 5 == 0
    puts "Fizzbuzz"
-end 
+
  elsif input % 3 == 0
    puts "Fizz"
-end   
+ 
  elsif input % 5 == 0 
    puts "Buzz"
 end
