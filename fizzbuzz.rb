bb91348049555d8eaa2f69ae039ba27681e0d90a
Jacob W Jones <jacobwadejones76@gmail.com>
2020-02-11 18:31:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,12 @@
 def fizzbuzz(fizz)
- if    3
-  puts "Fizz"
- elsif 5
-  puts "Buzz"
- elsif 15
-  puts "Fizzbuzz"
- else
-  puts nil 
-end
+   if    3
+    puts "Fizz"
+   elsif 5
+    puts "Buzz"
+   elsif 15
+    puts "Fizzbuzz"
+   else
+    puts nil 
+
+  end
+end 
