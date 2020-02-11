d04350b398cb6cb91d2027462497dd97d9c6cd9a
Jacob W Jones <jacobwadejones76@gmail.com>
2020-02-11 18:50:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,12 @@
+def fizzbuzz(any)
+  if any % 3 == 0 && any % 5 == 0
+      val="FizzBuzz"
+    elsif any % 3 == 0
+        val="Fizz"
+      elsif any % 5 == 0
+          val="Buzz"
+    else
+      puts "Not divisible by 3 or 5"
+  end 
 
+end
